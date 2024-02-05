92f8a525d469fb92207c07c29dbead15dcf520b1
fabroz.dev <fabroz.dev@gmail.com>
2024-02-05 14:01:45 +0100

    CHORE: vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,11 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
+// DEPLOY: https://youtu.be/dR9Swhw0L50?si=MfILbhd97tVfWFyp
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-  ],
+  plugins: [vue(), vueJsx()],
+  base: '/portfolio/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
